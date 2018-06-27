1601a0b25a94d7ae0f9e729ab7494a587b9fcbcc
brooks-charlie <charlie_brooks1996@yahoo.com>
2018-06-26 21:50:34 -0500

    application controller

--- src/ManageDB.java
+++ src/ManageDB.java
@@ -1,16 +1,19 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class ManageDB {
 
-    //public SessionFactory createFactory(){
-    //    SessionFactory factory;
-    //    try {
-    //        factory = new Configuration()
-    //                .configure("hibernate.cfg.xml")
-    //                .addAnnotatedClass(Student.class)
-    //                .buildSessionFactory();
-    //    } catch (Throwable ex) {
-    //        System.err.println("Failed to create sessionFactory object." + ex);
-    //        throw new ExceptionInInitializerError(ex);
-    //    }
-    //    return factory;
-   // }
+    public SessionFactory createFactory(){
+        SessionFactory factory;
+        try {
+            factory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Student.class)
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        return factory;
+    }
 }
