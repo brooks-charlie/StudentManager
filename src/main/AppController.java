2068d5f7fe52a8b64daaa4239d056e97b1a45e96
brooks-charlie <charlie_brooks1996@yahoo.com>
2018-07-15 00:27:22 -0500

    project

--- src/main/AppController.java
+++ src/main/AppController.java
@@ -10,7 +10,7 @@ public class AppController extends Thread {
     public AppController(){
         PopulaceControllerHash.put("Student", new Student());
         PopulaceControllerHash.put("Faculty", new Faculty());
-
+//comment
     }
 
 }
